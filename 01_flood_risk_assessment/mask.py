65f950f06526f9c88b3a07ce40359bd861afd69a
Hsu0734 <xuhwmy@gmail.com>
2024-07-17 00:25:46 +0800

    0514

--- 01_flood_risk_assessment/mask.py
+++ 01_flood_risk_assessment/mask.py
@@ -1,5 +1,5 @@
 """
-Read DEM data
+flow accumulation analysis
 Author: Hanwen Xu
 Date: Oct 01, 2023
 """
@@ -10,22 +10,36 @@ from rasterio.plot import show
 import matplotlib.pyplot as plt
 import numpy as np
 
-
-
+plt.rcParams['font.size'] = 20
 wbe = wbw.WbEnvironment()
 wbe.verbose = False
 wbe.working_directory = r'D:\PhD career\05 SCI papers\08 Topographic modification optimization' \
                         r'\FloodRisk_optimization\00_data_source'
 
-# web read DEM data
-dem = wbe.read_raster('buffer reclass.tif')
 
-# resample
-resample = wbe.resample(input_rasters=[dem], cell_size=5.0)
-wbe.write_raster(resample, 'Hanwen_5m.tif', compress=True)
+# web read DEM data
+dem = wbe.read_raster('Hanwen_5m.tif')
+mask = wbe.read_raster('buffer_reclass.tif')
+
+# slope analysis
+new_mask = wbe.new_raster(dem.configs)
+
+for row in range(dem.configs.rows):
+    for col in range(dem.configs.columns):
+        elev = dem[row, col]
+        mask_elev = mask[row, col]
+        if mask_elev == 0.0:
+            new_mask[row, col] = dem.configs.nodata
+        elif elev == dem.configs.nodata:
+            new_mask[row, col] = dem.configs.nodata
+        else:
+            new_mask[row, col] = 1.0
+
+wbe.write_raster(new_mask, 'Hanwen_mask.tif', compress=True)
+
 
 # visualization
-path_01 = '../00_data_source/Hanwen_5m.tif'
+path_01 = '../00_data_source/Hanwen_mask.tif'
 data_01 = rs.open(path_01)
 
 dem_array = data_01.read(1, masked=True)  # 使用 masked=True 来自动处理 nodata 值
@@ -39,7 +53,7 @@ show(data_01, ax=ax)
 
 # Create a colorbar
 cbar = plt.colorbar(image, ax=ax, orientation='vertical', shrink=0.5)
-cbar.set_label('Elevation (meters)')
+cbar.set_label('Hanwen_mask')
 
 plt.ticklabel_format(style='plain')
 ax.grid(True, linestyle='--', color='grey')
